082c5f46a84d2b2a3e8e7946943d8c046428077a
dakotawalterz <wal19011@byui.edu>
2022-11-01 13:15:36 -0600

    fallmovement addition
    Added the Fallmovement class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Text;
+using 
+
+
+namespace FallingObjects
+{
+    public class Forml : [greed]
+    {
+
+        bool goleft;
+        bool goright
+        bool goup
+        bool godown
+        int speed = 4
+        Random rndY = new Random
+        Random rndX = new Random
+        public [greed]
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
