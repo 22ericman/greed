38cf612593d686ed501e913b9851c2d5572a9517
dakotawalterz <wal19011@byui.edu>
2022-11-01 13:28:58 -0600

    Update FallMovement.cs

--- FallMovement.cs
+++ FallMovement.cs
@@ -1,13 +1,10 @@
 
 public class falling
-    {
-
-        bool goleft;
-        bool goright;
-        bool goup;
-        bool godown;
-        int speed = 4;
+{
+        int fallspeed = 4;
         Random rndY = new Random();
         Random rndX = new Random();
         public falling
-    }    
+    
+}
+
