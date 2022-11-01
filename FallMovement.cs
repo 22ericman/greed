71a75926ade06a91ed4a9a0b87a4dd46adc2ad4b
dakotawalterz <wal19011@byui.edu>
2022-11-01 13:24:37 -0600

    Moving everything to program

---
+++ FallMovement.cs
@@ -0,0 +1,13 @@
+
+public class falling
+    {
+
+        bool goleft;
+        bool goright;
+        bool goup;
+        bool godown;
+        int speed = 4;
+        Random rndY = new Random();
+        Random rndX = new Random();
+        public falling
+    }    
